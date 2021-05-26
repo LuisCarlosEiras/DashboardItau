1beab2f21ec906da24f7cde3496e942dac6da1ca
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2021-05-26 07:55:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ st.plotly_chart(grafico2)
 
 # Referências
 st.write("""Referência: Mini Projeto 2, Data App, Dashboard Financeiro Interativo e em Tempo Real Para Previsão de Ativos Financeiros, 
-Curso Visualização de Dados e Design de Dashboards, Data Science Academy""")
+Curso Visualização de Dados e Design de Dashboards, Data Science Academy.""")
 
 # Fim
 
